fbcacaff1f3a0b724fad77e978d15340d72f375d
BayUzman <44754274+BayUzman@users.noreply.github.com>
2018-11-04 22:20:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -320,7 +320,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('Aleyküm selam,  hoş geldin KNK');
+    msg.reply('Aleyküm selam,  hoşgeldin Knk');
   }
 });
 
